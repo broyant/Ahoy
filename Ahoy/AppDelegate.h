bd0ae3512d9dd2063bfe23cefa0a08ae32e5ba10
lichuanjun <lichuanjun1991@gmail.com>
2015-10-11 18:23:04 +0800

    create repository

---
+++ Ahoy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ahoy
+//
+//  Created by lichuanjun on 11/10/15.
+//  Copyright © 2015 Ahoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
